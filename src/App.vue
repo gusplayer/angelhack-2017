fa6eef1d97f89563ef7504755701cd6ea855708d
Diego Aguilar <daafaguilar@gmail.com>
2017-05-27 18:36:48 -0500

    Diseños generales y empieza login

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,41 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    padding: 0px;
+    margin: 0px;
+  }
+  .screen-padding{
+    padding: 30px;
+  }
+  .input{
+    max-width: 300px;
+    width: 100%;
+    border-style: none;
+    border: 1px solid #FFF;
+    padding: 10px;
+    margin: 10px;
+    background-color: transparent;
+    color: #FFF;
+  }
+  .input::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+  color: #FFF;
+  }
+  .input::-moz-placeholder { /* Firefox 19+ */
+    color: #FFF;
+  }
+  .input:-ms-input-placeholder { /* IE 10+ */
+    color: #FFF;
+  }
+  .input:-moz-placeholder { /* Firefox 18- */
+    color: #FFF;
+  }
+
+  .btnAction{
+    color: #FFF;
+    background-color: red;
+    text-decoration: none;
+    padding: 10px 60px;
+    border-radius: 25px;
+  }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,3 +1,29 @@
 <template>
-	<h1>login</h1>
+	<div class="login">
+		<figure class="login_logo">
+			<img src="">
+		</figure>
+		<p class="login_text">Bienvenido a rappi-dito la aplicacion con la que podras hacer uso de nuestros rappi-tenderos para enviar tus productos a tus clientes.</p>
+		<div class="login_form">
+			<input type="email" placeholder="Correo" class="input">
+			<input type="password" placeholder="Contraseña" class="input">
+			<router-link to="in" class="btnAction">Login</router-link>
+		</div>
+	</div>
 </template>
+<style scoped>
+	.login{
+		width: 100%;
+		height: 100vh;
+		background-color: #ff5845;
+	}
+	.login_text{
+		color: #FFF;
+	}
+	.login_form{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>

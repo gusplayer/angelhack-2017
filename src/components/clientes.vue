77ada1a4993b93151d1da540568536c08e7eb6be
Diego Aguilar <daafaguilar@gmail.com>
2017-05-28 00:16:54 -0500

    Creando vista de clientes

--- src/components/clientes.vue
+++ src/components/clientes.vue
@@ -1,15 +1,36 @@
 <template>
-	<div class="clientes">
-		<div class="cliente_card">
-			<div></div>
+	<div class="clientes screen_padding">
+		<router-link to="nuevo-cliente" class="btnAction">Crear Cliente</router-link>
+		<router-link to="clientes/2" class="cliente_card" v-for="customer in customers" >
 			<div>
-				<p>Gustavo Moreno</p>
-				<p>3123464232</p>
-				<p>Cll 15 #43c - 23</p>
+				<span></span>
 			</div>
-		</div>
+			<div>
+				<p>{{customer.name}}</p>
+				<p>{{customer.phonenumber}}</p>
+				<p>{{customer.address}}</p>
+			</div>
+		</router-link>
 	</div>
 </template>
+<script scoped>
+	export default{
+		firebase: {
+			customers: firebase.database().ref('/customers')
+		}
+	}
+</script>
 <style scoped>
-	
+	.clientes{
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.cliente_card{
+		box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.15);
+		padding: 10px 20px;
+		margin: 10px;
+	}
 </style>
--- src/components/nuevo-cliente.vue
+++ src/components/nuevo-cliente.vue
@@ -1,3 +1,12 @@
 <template>
-	<h1>Nuevo Cliente</h1>
+	<div class="nuevo_cliente">
+		<div class="nuevo_cliente_form">
+			<input type="text" placeholder="Nombre del cliente">
+			<input type="text" placeholder="Direccion">
+			<input type="text" placeholder="Numero de celular">
+		</div>
+	</div>
 </template>
+<style>
+	
+</style>
